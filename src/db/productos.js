1b3ba595244326df187c2227ca5344f36e819e9d
Cristian Gobbesso <cristiangobbesso.it@gmail.com>
2025-01-19 16:55:39 -0300

    Agregue unos productos mas y pase todas las imagenes a webp

--- src/db/productos.js
+++ src/db/productos.js
@@ -2,38 +2,64 @@ const productos = [
   { 
     id: 1, 
     nombre: "Iphone 11 Pro Max", 
-    foto: "imgs/apple-iphone-11-pro-max.jpg", 
+    foto: "imgs/apple-iphone-11-pro-max.webp", 
     descripcion: "Iphone 11 super pro max", 
     precio: 222.20
   },
   { 
     id: 2, 
     nombre: "IPhone 12", 
-    foto: "imgs/apple-iphone-12.jpg", 
+    foto: "imgs/apple-iphone-12.webp", 
     descripcion: "Iphone 12, flojito", 
     precio: 333.30
   },
   { 
     id: 3, 
     nombre: "Camara Canon", 
-    foto: "imgs/camara-canon.jpg", 
+    foto: "imgs/camara-canon.webp", 
     descripcion: "Camara ultra 4k", 
     precio: 444.40
   },
   { 
     id: 4, 
     nombre: "Drone DJI", 
-    foto: "imgs/drone-dji.jpg", 
+    foto: "imgs/drone-dji.webp", 
     descripcion: "Alto drone", 
     precio: 555.50
   },
   { 
     id: 5, 
     nombre: "Auriculares Sony", 
-    foto: "imgs/auriculares-sony.jpg", 
+    foto: "imgs/auriculares-sony.webp", 
     descripcion: "Cancelación activa!", 
     precio: 666.60
-  }
+  },
+
+  
+  {  
+    id: 6, 
+    nombre: "Celular Samsung", 
+    foto: "imgs/celular-samsung.webp", 
+    descripcion: "el mejor celular ", 
+    precio: 150.000
+  },
+  
+  {  
+    id: 7, 
+    nombre: "Modem Motorola", 
+    foto: "imgs/modem-motorola.webp", 
+    descripcion: "Modem wifi ", 
+    precio: 35.800
+  },
+
+  {  
+    id: 7, 
+    nombre: "Notebook Lenovo 2022", 
+    foto: "imgs/notebook-lenovo.webp", 
+    descripcion: "Notebook para oficina ", 
+    precio: 35.800
+  },
+
 ]
 
 export default productos
