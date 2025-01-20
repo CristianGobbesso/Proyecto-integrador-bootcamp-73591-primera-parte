3c00cb14167bbc951e0b7b02865df313d84a1d31
Cristian Gobbesso <cristiangobbesso.it@gmail.com>
2025-01-20 18:50:42 -0300

    agregue la descripcion a los productos

--- src/db/productos.js
+++ src/db/productos.js
@@ -3,44 +3,44 @@ const productos = [
     id: 1, 
     nombre: "Iphone 11 Pro Max", 
     foto: "imgs/apple-iphone-11-pro-max.webp", 
-    descripcion: "Iphone 11 super pro max", 
+    descripcion: "Pantalla Super Retina XDR, triple cámara, rendimiento potente y diseño premium.", 
     precio: 222.20
   },
   { 
     id: 2, 
     nombre: "IPhone 12", 
     foto: "imgs/apple-iphone-12.webp", 
-    descripcion: "Iphone 12, flojito", 
+    descripcion: "Diseño elegante, pantalla OLED, rendimiento A14 Bionic, y cámara dual avanzada.", 
     precio: 333.30
   },
   { 
     id: 3, 
     nombre: "Camara Canon", 
     foto: "imgs/camara-canon.webp", 
-    descripcion: "Camara ultra 4k", 
+    descripcion: "Captura profesional, alta resolución, enfoque rápido y conectividad avanzada.", 
     precio: 444.40
   },
   { 
     id: 4, 
     nombre: "Drone DJI", 
     foto: "imgs/drone-dji.webp", 
-    descripcion: "Alto drone", 
+    descripcion: "Vuelo estable, fácil manejo, cámara integrada y diseño portátil.", 
     precio: 555.50
   },
   { 
     id: 5, 
     nombre: "Auriculares Sony", 
     foto: "imgs/auriculares-sony.webp", 
-    descripcion: "Cancelación activa!", 
+    descripcion: "Sonido envolvente, calidad premium, cómodos y con cancelación de ruido.", 
     precio: 666.60
   },
 
   
   {  
     id: 6, 
-    nombre: "Celular Samsung", 
+    nombre: "Celular Samsung S22", 
     foto: "imgs/celular-samsung.webp", 
-    descripcion: "el mejor celular ", 
+    descripcion: "Diseño moderno, cámara impresionante, rendimiento potente y pantalla AMOLED vibrante. ", 
     precio: 150.000
   },
   
@@ -48,7 +48,7 @@ const productos = [
     id: 7, 
     nombre: "Modem Motorola", 
     foto: "imgs/modem-motorola.webp", 
-    descripcion: "Modem wifi ", 
+    descripcion: "Modem wifi conexión rápida, tecnología avanzada, y configuración sencilla. ", 
     precio: 35.800
   },
 
@@ -56,7 +56,7 @@ const productos = [
     id: 7, 
     nombre: "Notebook Lenovo 2022", 
     foto: "imgs/notebook-lenovo.webp", 
-    descripcion: "Notebook para oficina ", 
+    descripcion: "Rendimiento versátil, diseño ligero, pantalla nítida y batería de larga duración. ", 
     precio: 35.800
   },
 
