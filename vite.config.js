867802e1f746c6a7788fb9fc3fc80dbd7a724dd3
Cristian Gobbesso <cristiangobbesso.it@gmail.com>
2025-02-10 23:15:19 -0300

    cree la ruta para conectar el nosotros y contacto al index

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,8 @@ export default {
     build: {
         rollupOptions: {
             input: resolve(__dirname, 'index.html'),
-            nosotros: resolve(__dirname, 'src/pages/nosotros.html'),
-            contacto: resolve(__dirname, 'src/pages/contacto.html')
+            nosotros: resolve(__dirname, './src/pages/nosotros.html'),
+            contacto: resolve(__dirname, './src/pages/contacto.html')
         }
     }
 }
